20a7648005db33644ea45ca6c6e1d59be2b52674
Rakesh Muppa <RAMuppa+vitech@vitechinc.com>
2026-08-04 13:45:10 +0530

    feat(option-target): expose POST /api/v1/optiontarget

--- restx_api/__init__.py
+++ restx_api/__init__.py
@@ -37,6 +37,7 @@ from .openposition import api as openposition_ns
 from .option_chain import api as option_chain_ns
 from .option_greeks import api as option_greeks_ns
 from .option_symbol import api as option_symbol_ns
+from .option_target import api as option_target_ns
 from .options_multiorder import api as options_multiorder_ns
 from .options_order import api as options_order_ns
 from .orderbook import api as orderbook_ns
@@ -47,10 +48,10 @@ from .place_order import api as place_order_ns
 from .place_smart_order import api as place_smart_order_ns
 from .pnl_symbols import api as pnl_symbols_ns
 from .portfolio import api as portfolio_ns
-from .sip import api as sip_ns
 from .positionbook import api as positionbook_ns
 from .quotes import api as quotes_ns
 from .search import api as search_ns
+from .sip import api as sip_ns
 from .split_order import api as split_order_ns
 from .symbol import api as symbol_ns
 from .synthetic_future import api as synthetic_future_ns
@@ -73,6 +74,7 @@ api.add_namespace(portfolio_ns, path="/portfolio")
 api.add_namespace(sip_ns, path="/sip")
 api.add_namespace(depth_ns, path="/depth")
 api.add_namespace(option_chain_ns, path="/optionchain")
+api.add_namespace(option_target_ns, path="/optiontarget")
 api.add_namespace(intervals_ns, path="/intervals")
 api.add_namespace(funds_ns, path="/funds")
 api.add_namespace(orderbook_ns, path="/orderbook")
